f5423e8107a4b1842e86edfce3a48fabd4f59173
devyanisharma <sharmadevyani7796@gmail.com>
2023-05-01 18:11:09 +0530

    JWT Implementation

---
+++ springdemojwt/src/test/java/com/security/springdemojwt/SpringdemojwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springdemojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemojwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
